1ca3d6fa4e0e1f3712ef247709931c109542fc32
Christopher Nethercott <ccnethercott@gmail.com>
2018-07-22 11:25:00 +0100

    Added BasicPlugin main class with a comment

---
+++ src/main/java/com/arwenmc/BasicPlugin.java
@@ -0,0 +1,17 @@
+package com.arwenmc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BasicPlugin extends JavaPlugin { // The main plugin class should always have the same name as the project and never be called Main.
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
